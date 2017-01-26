662cc7cee23b55a64c6d6b633604dcd2fc6a7ad5
Matthew Paul Macneil <mmacneil@tesla22.eecs.utk.edu>
2017-01-26 11:13:29 -0500

    fixed errors with cases

--- printf.cpp
+++ printf.cpp
@@ -21,31 +21,44 @@ int printf(const char *fmt, ...)
         {
             //decimals, use int64_t
             case 'd':
-			   int64_t a_decimal_parameter = va_arg(args, int64_t);
+			{
+			   int64_t decimal_parameter = va_arg(args, int64_t);
 			   /*so to use write, must convert int64_t to a char * this can be confusing so im just writing comments as I understand it before implementing it
 			   If you have a number 12345 and divide by 10000 i get 1.2345. Then %10 that number and you get 1. 
 			   Then you divide  12345 by 1000 and get 12.345, then %10 and you get 2. Add that to the char *, should have 12. Keep going until its 12345 %10 which is 5 so you youll have the char * of 12345 */
 			   write(1, "this be d", 10);
                break;
+			}
            //floats, use double
             case 'f':
-			   double a_double_parameter = va_arg(args, double);
+			{
+			   double double_parameter = va_arg(args, double);
 			   write(1, "this be f", 10);
                break;
+			}
             //hex, use uint64_t
             case 'x':
-			   uint64_t a_hex_parameter = va_arg(args, uint64_t);
+			{
+			   uint64_t hex_parameter = va_arg(args, uint64_t);
 			   write(1, "this be x", 10);
 			   break;
+			}
 			//EXTRA CREDIT. string, use char *
 			case 's':
-			   char * a_string_parameter = va_arg(args, char *);
+			{
+			   char * string_parameter = va_arg(args, char *);
 			   //get length of the char * then print that out using write(1, char *, length)
   		       break;
+			}
             default:
 			 //  write(1,c,1);
 			   break;
         }
+	  }
+	  else
+	  {
+		//here we write out anything not specified as a type of variable
+		//write(1, c, 1);
 	  }
    }
 
