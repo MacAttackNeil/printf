d24bbec3c18b48159c6d54d608965bf72e2a9e2f
Macbrine <mmacneil@vols.utk.edu>
2017-01-26 11:07:18 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "printf.hpp"
+
+int main(int argc, char **argv)
+{
+   printf("This is an integer : %d", 2);
+   printf("This is a float : %f", 6.7);
+   printf("This is a hexadecimal num : %x", 43);
+   //printf("This is a string : %s", "Sup");
+   
+   return 0;
+}
